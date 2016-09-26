9b159c984b4080d5ccf356979da974c8c031a7d1
Andherson Maeda <maeda.br@gmail.com>
2016-09-26 17:11:10 -0300

    Added Spring test dependency

---
+++ src/test/java/com/example/beans/lifecycle/LifecycleExampleBeanTest.java
@@ -0,0 +1,23 @@
+package com.example.beans.lifecycle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=LifecycleExampleBean.class)
+public class LifecycleExampleBeanTest {
+
+    @Autowired
+    private LifecycleExampleBean bean;
+
+    @Test
+    public void lifecycleTest() {
+        assertNotNull(bean);
+    }
+
+}
