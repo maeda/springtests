8faaafd288b9f27baa97b4bad442384d32da74ca
Andherson Maeda <maeda.br@gmail.com>
2016-09-27 13:44:04 -0300

    Conditional Bean examples.

--- src/main/java/com/example/beans/conditional/ConditionalExampleBean.java
+++ src/main/java/com/example/beans/conditional/ConditionalExampleBean.java
@@ -2,6 +2,13 @@ package com.example.beans.conditional;
 
 import org.springframework.stereotype.Component;
 
+/**
+ * A good example about how to use the condition is @Profile annotation. That profile have a @Conditional annotation
+ * that point to ProfileCondition class where have the logic to decide bean creation by profile.
+ *
+ * @see org.springframework.context.annotation.Profile
+ * @see org.springframework.context.annotation.ProfileCondition
+ */
 @Component
 public class ConditionalExampleBean {
 
